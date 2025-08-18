18b206fb3a72bd5c274e767f52f04d39e2405c2a
Felipe <rocha.felipe98@gmail.com>
2025-08-18 11:19:19 -0300

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_07_22_193643) do
-
   create_table "produtos", force: :cascade do |t|
     t.string "name"
-    t.float "price"
+    t.to_f "price"
     t.string "photo"
     t.boolean "is_promotion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
 end
